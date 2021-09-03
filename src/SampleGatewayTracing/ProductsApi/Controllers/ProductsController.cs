870d8d9809e139725b684cf90d820642e28f11cc
Dumindu <dumindukm@gmail.com>
2021-09-03 22:51:10 +0530

    initial commit

---
+++ src/SampleGatewayTracing/ProductsApi/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductsApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(ILogger<ProductsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<ProductViewModel> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new ProductViewModel
+            {
+                Id = index,
+                Name = "p"+index
+            })
+            .ToArray();
+        }
+    }
+}
